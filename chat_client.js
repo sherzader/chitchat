4f9eb3119c03921d0208a3f1c4f3ccf69770efe5
Sheri Zada <sheri.h.zada@gmail.com>
2016-05-02 16:15:27 -0700

    add username input in html

--- chat_client.js
+++ chat_client.js
@@ -6,10 +6,15 @@ $(document).ready(function() {
     socket.emit('chat message', $('#m').val());
     $('#m').val('');
   });
+
+  $('#name').submit(function(e) {
+    socket.emit('new user', $('#name').val());
+    $('#name').val('');
+  });
 });
+
 socket.on('chat message', function(msg) {
-  console.log(msg);
   var now = new Date();
-  var timestring = now.getHours() + ':' + now.getMinutes();
+  var timestring = ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2);
   $('#messages').append($('<li>').text('< ' + timestring + ' > ' + msg));
 });
