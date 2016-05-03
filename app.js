1b0e5e3be0c8b2953e1c59a25976230ffb64b552
Sheri Zada <sheri.h.zada@gmail.com>
2016-05-02 20:02:26 -0700

    io.emit() fixes updates rendering in multiple windows

--- app.js
+++ app.js
@@ -10,31 +10,31 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+// io.on('connection', function(socket){
+//   socket.on('chat message', function(msg){
+//     io.emit('chat message', msg);
+//   });
+// });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
 
-io.sockets.on('connection', function(socket) {
+io.on('connection', function(socket) {
   socket.on('join', function(name) {
     people[socket.id] = name;
-    socket.emit('update', 'You have connected to the server.');
-    socket.emit('update', name + ' has joined the server.');
-    socket.emit('update-people', people);
+    io.emit('update', 'You have connected to the server.');
+    io.emit('update', name + ' has joined the server.');
+    io.emit('update-people', people);
   });
 
   socket.on('send', function(msg) {
-    socket.emit('chat', people[socket.id], msg);
+    io.emit('chat', people[socket.id], msg);
   });
 
   socket.on('disconnect', function() {
-    socket.emit('update', people[socket.id] + ' has left.');
+    io.emit('update', people[socket.id] + ' has left.');
     delete people[socket.id];
-    socket.emit('update-people', people);
+    io.emit('update-people', people);
   });
 });
--- chat_client.js
+++ chat_client.js
@@ -1,7 +1,5 @@
 var socket = io();
 
-var people = {};
-
 $(document).ready(function() {
   $('form#msg').submit(function(e) {
     e.preventDefault();
@@ -14,49 +12,29 @@ $(document).ready(function() {
     socket.emit('join', $('#name').val());
     $('#name').val('');
   });
+});
 
-  socket.on('chat', function(who, msg) {
-    var now = new Date();
-    var timestring = ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2);
-    $('#messages').append($('<li>').text('< ' + timestring + ' > ' + who + ': ' + msg));
-  });
+socket.on('chat', function(who, msg) {
+  var now = new Date();
+  var timestring = ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2);
+  $('#messages').append('<li>< ' + timestring + ' > <strong>' + who + '</strong> : ' + msg + '</li>');
+});
 
-  socket.on('update', function(msg) {
-    var now = new Date();
-    var timestring = ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2);
-    $('#messages').append($('<li>').text('< ' + timestring + ' > ' + msg));
-  });
+socket.on('update', function(msg) {
+  var now = new Date();
+  var timestring = ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2);
+  $('#messages').append($('<li>').text('< ' + timestring + ' > ' + msg));
+});
 
-  socket.on('update-people', function(people) {
-    $('#people').empty();
-    $.each(people, function(clientid, name) {
-      $('#people').append(name);
-    });
-  });
-
-  socket.on('disconnect', function() {
-    var now = new Date();
-    var timestring = ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2);
-    $('#messages').append($('<li>').text('< ' + timestring + ' > The server is not available.'));
+socket.on('update-people', function(people) {
+  $('#people').empty();
+  $.each(people, function(clientid, name) {
+    $('#people').append(name + '<br />');
   });
 });
 
-
-socket.on('connection', function(client) {
-  client.on('join', function(name) {
-    people[client.id] = name;
-    client.emit('update', 'You have connected to the server.');
-    socket.sockets.emit('update', name + ' has joined the server.');
-    socket.sockets.emit('update-people', people);
-  });
-
-  client.on('send', function(msg) {
-    socket.sockets.emit('chat', people[client.id], msg);
-  });
-
-  client.on('disconnect', function() {
-    socket.sockets.emit('update', people[client.id] + ' has left.');
-    delete people[client.id];
-    socket.sockets.emit('update-people', people);
-  });
+socket.on('disconnect', function() {
+  var now = new Date();
+  var timestring = ('0' + now.getHours()).slice(-2) + ':' + ('0' + now.getMinutes()).slice(-2);
+  $('#messages').append($('<li>').text('< ' + timestring + ' > The server is not available.'));
 });
